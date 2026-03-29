687e342faf0e6afec3971216256cc095ca86760d
sauravsapkota378-stack <sauravsapkota378@gmail.com>
2026-03-29 09:01:11 +0545

    Adding Subtraction

---
+++ Subtraction.py
@@ -0,0 +1,13 @@
+# Subtraction Calculator
+
+# Ask the user for the first number
+num1 = float(input("Enter the first number: "))
+
+# Ask the user for the second number
+num2 = float(input("Enter the second number: "))
+
+# Perform subtraction
+result = num1 - num2
+
+# Display the result
+print("The subtraction result is:", result)
